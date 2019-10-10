946b1e9625f110198877f8177ca03a124634b7d9
kuxidaica <hth0901@gmail.com>
2019-10-10 07:35:47 +0700

    ~~

--- SuccessAppService/Controllers/SemesterController.cs
+++ SuccessAppService/Controllers/SemesterController.cs
@@ -43,5 +43,87 @@ namespace SuccessAppService.Controllers
                 return res;
             });
         }
+
+        [Route("update")]
+        [HttpPut]
+        public HttpResponseMessage updateSemester(HttpRequestMessage req, eSemester semester)
+        {
+            DateTime mDate = DateTime.ParseExact(semester.OPENING_DATE, "dd/MM/yyyy", null);
+            semester.OPENING_DATE = mDate.ToString("yyyyMMdd");
+            mDate = DateTime.ParseExact(semester.REGISTER_FROM, "dd/MM/yyyy", null);
+            semester.REGISTER_FROM = mDate.ToString("yyyyMMdd");
+            mDate = DateTime.ParseExact(semester.REGISTER_TO, "dd/MM/yyyy", null);
+            semester.REGISTER_TO = mDate.ToString("yyyyMMdd");
+            return CreateHttpResponse(req, () =>
+            {
+                HttpResponseMessage res = null;
+                if (!ModelState.IsValid)
+                {
+                    res = req.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                    return res;
+                }
+                DataTable dtResult = aSemesterAccess.updateSemester(semester);
+                eResult insertResult = new eResult
+                {
+                    dbResult = dtResult.Rows[0][0].ToString(),
+                    dbMessage = dtResult.Rows[0][1].ToString()
+                };
+                res = req.CreateResponse(HttpStatusCode.Created, insertResult);
+                return res;
+            });
+        }
+
+        [Route("getallsemester")]
+        [HttpGet]
+        public HttpResponseMessage getDetailSemester(HttpRequestMessage req)
+        {
+            return CreateHttpResponse(req, () =>
+            {
+                HttpResponseMessage res = null;
+                DataTable dtResult = aSemesterAccess.semesterQuery();
+                List<eSemester> lsResult = new List<eSemester>();
+
+                for (int i = 0; i < dtResult.Rows.Count; i++)
+                {
+                    eSemester eResult = new eSemester
+                    {
+                        SMT_ID = dtResult.Rows[i]["SMT_ID"].ToString(),
+                        SMT_NAME = dtResult.Rows[i]["SMT_NAME"].ToString(),
+                        OPENING_DATE = dtResult.Rows[i]["OPENING_DATE"].ToString(),
+                        REGISTER_FROM = dtResult.Rows[i]["REGISTER_FROM"].ToString(),
+                        REGISTER_TO = dtResult.Rows[i]["REGISTER_TO"].ToString(),
+                        SMT_DESC = dtResult.Rows[i]["SMT_DESC"].ToString(),
+                        REMARK = dtResult.Rows[i]["REMARK"].ToString()
+                    };
+                    lsResult.Add(eResult);
+                }
+                res = req.CreateResponse(HttpStatusCode.OK, lsResult);
+                return res;
+            });
+        }
+
+        [Route("getdetail")]
+        [HttpGet]
+        public HttpResponseMessage getDetailSemester(HttpRequestMessage req, string semesterCode)
+        {
+            return CreateHttpResponse(req, () =>
+            {
+                HttpResponseMessage res = null;
+                DataTable dtResult = aSemesterAccess.getDetailTest();
+
+                eSemester eResult = new eSemester
+                {
+                    SMT_ID = dtResult.Rows[0]["SMT_ID"].ToString(),
+                    SMT_NAME = dtResult.Rows[0]["SMT_NAME"].ToString(),
+                    OPENING_DATE = dtResult.Rows[0]["OPENING_DATE"].ToString(),
+                    REGISTER_FROM = dtResult.Rows[0]["REGISTER_FROM"].ToString(),
+                    REGISTER_TO = dtResult.Rows[0]["REGISTER_TO"].ToString(),
+                    SMT_DESC = dtResult.Rows[0]["SMT_DESC"].ToString(),
+                    REMARK = dtResult.Rows[0]["REMARK"].ToString()
+                };
+                res = req.CreateResponse(HttpStatusCode.OK, eResult);
+                return res;
+            });
+        }
     }
 }
--- SuccessAppService/Framework/Access/aSemesterAccess.cs
+++ SuccessAppService/Framework/Access/aSemesterAccess.cs
@@ -24,5 +24,43 @@ namespace SuccessAppService.Framework.Access
             DataTable dtResult = DBHelper.getDataTable_SP(spName, param);
             return dtResult;
         }
+
+        public static DataTable updateSemester(eSemester semester)
+        {
+            string spName = "SP_T_SEMESTER_UPDATE";
+            SqlParameter[] param = new SqlParameter[7];
+            param[0] = new SqlParameter("@SMT_ID", semester.SMT_ID);
+            param[1] = new SqlParameter("@PNAME", semester.SMT_NAME);
+            param[2] = new SqlParameter("@POPENDATE", semester.OPENING_DATE);
+            param[3] = new SqlParameter("@PREGFROM", semester.REGISTER_FROM);
+            param[4] = new SqlParameter("@PREGTO", semester.REGISTER_TO);
+            param[5] = new SqlParameter("@PDESC", semester.SMT_DESC);
+            param[6] = new SqlParameter("@PREMARK", semester.REMARK);
+            DataTable dtResult = DBHelper.getDataTable_SP(spName, param);
+            return dtResult;
+        }
+
+        public static DataTable getDetailTest()
+        {
+            string spName = "SP_T_SEMESTER_detail_test";
+            DataTable dtResult = DBHelper.getDataTable_SP(spName, null);
+            return dtResult;
+        }
+
+        public static DataTable getDetailSemester(string smtCode)
+        {
+            string spName = "SP_T_SEMESTER_UPDATE";
+            SqlParameter[] param = new SqlParameter[1];
+            param[0] = new SqlParameter("@SMT_ID", smtCode);
+            DataTable dtResult = DBHelper.getDataTable_SP(spName, param);
+            return dtResult;
+        }
+
+        public static DataTable semesterQuery()
+        {
+            string spName = "SP_T_SEMESTER_QUERY_ALL";
+            DataTable dtResult = DBHelper.getDataTable_SP(spName, null);
+            return dtResult;
+        }
     }
 }
